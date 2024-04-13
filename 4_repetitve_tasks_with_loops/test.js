cac3b9351121971742f293d0968e914d9b8c0774
Med-benabdallah <mouhamed.benabdallah@esprit.tn>
2024-04-13 19:44:55 +0100

    new exercice

---
+++ 4_repetitve_tasks_with_loops/test.js
@@ -0,0 +1,23 @@
+for (var i = 0; i <10; i++) {
+    console.log(i+1);
+}
+console.log("you learned how to count from 1 to 10");
+
+for (var i= 10; i > 0; i--) {
+    console.log(i);
+}
+console.log("Happy Birthday");
+
+var i = 0;
+while (i < 10) {
+    console.log(i+1);
+    i++;
+}
+console.log("You learned how to count from 1 to 10");
+
+var i = 10;
+while (i > 0) {
+    console.log(i);
+    i--;
+}
+console.log("Happy Birthday");
