1d03c68727dbc02a92b3233ebd65ceaf74ca33b3
Med-benabdallah <mouhamed.benabdallah@esprit.tn>
2024-04-15 16:14:50 +0100

    new exercice

---
+++ 7_arrays_and_objects/test.js
@@ -0,0 +1,20 @@
+var clothes = [];
+clothes.push("jacket");
+clothes.push("t-shirt");
+clothes.push("cargo pants");
+clothes.push("hoodies");
+clothes.push("shoes");
+clothes.pop();
+console.log(clothes);
+clothes.push("shoes");
+console.log(clothes);
+ 
+var favCar = {
+
+}
+
+favCar.make = "BMW";
+favCar.color = "black";
+favCar.Covertible = false;
+
+console.log(favCar);
---
+++ 8_error_handling_and prevention/test.js
@@ -0,0 +1,14 @@
+function addTwoNums(a,b){
+   try{
+    if(typeof(a) != 'number' || typeof(b) != 'number'){
+        console.log(e)
+    }
+    else{  console.log(a+b); }
+   }catch(e){
+     console.log("pls enter a number")
+   }
+    
+}
+
+addTwoNums(2,'3');
+addTwoNums(2,4); 
